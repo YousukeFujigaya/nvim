bcd361364be45e5a2fe2f6701cac2dbe18cbdb7c
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2023-04-17 01:23:06 +0900

    update

--- lua/default/config/cmp.lua
+++ lua/default/config/cmp.lua
@@ -144,7 +144,3 @@ cmp.setup {
     native_menu = false,
   },
 }
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
