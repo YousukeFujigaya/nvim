8a2f377c855861c6e984258308eb57a40c88d580
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2023-04-25 21:57:46 +0900

    update options and setup neodev

--- lua/default/settings/lsp/init.lua
+++ lua/default/settings/lsp/init.lua
@@ -1,7 +1,17 @@
-local status_ok, _ = pcall(require, 'lspconfig')
+local status_ok, lspconfig = pcall(require, 'lspconfig')
 if not status_ok then
   return
 end
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = 'Replace',
+      },
+    },
+  },
+}
 
 require 'default.settings.lsp.mason'
 require('default.settings.lsp.handlers').setup()
--- lua/user/settings/options.lua
+++ lua/user/settings/options.lua
@@ -1,61 +1,61 @@
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
+vim.opt.backup = false -- Create a backup file
+
 -- encoding
 vim.o.encofing = 'utf-8'
 vim.scriptencoding = 'utf-8'
 
--- Set highlight on search
-vim.o.hlsearch = true -- false -> true
+vim.opt.swapfile = false -- Create a swapfile
+vim.opt.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.undofile = true -- Enable persistent undo
+
+vim.opt.helplang = { 'ja', 'en' }
+vim.o.termguicolors = true -- Set term gui colors (most terminals support this)
+-- NOTE: You should make sure your terminal supports this
+vim.o.mouse = 'a' -- Allow the mouse to be used in neovim
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim
+vim.opt.cmdheight = 1 -- More space in the neovim command line for displaying messages
+vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.wo.number = true -- Make line numbers default
+vim.opt.relativenumber = false -- Set relative numbered lines
+vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.opt.cursorline = true -- Highlight the current line
+vim.o.hlsearch = true -- Highlight all matches on previous search pattern
 -- Tips: '<leader>h' = 'No highlight'
 --  [*] -> highlight a word on cursor
 --  'n','N' -> (highlight and) next or preveous the same word
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
+vim.o.ignorecase = true -- Ignore case in search patterns
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.opt.smartindent = true -- Make indenting smarter again
+vim.o.breakindent = true -- Enable break indent
+vim.opt.pumheight = 10 -- Pop up menu height
+vim.opt.showmode = false -- We don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 2 -- Always show tabs
+vim.opt.splitbelow = true -- Force all horizontal splits to go below current window
+vim.opt.splitright = true -- Force all vertical splits to go to the right of current window
+vim.wo.signcolumn = 'yes' -- Keep signcolumn on by default
 
 -- Decrease update time
-vim.o.updatetime = 250
+vim.o.updatetime = 300
 vim.o.timeout = true
-vim.o.timeoutlen = 500
+vim.o.timeoutlen = 300
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.opt.wrap = true -- display lines as one long line
+vim.opt.linebreak = true -- Companion to wrap, don't split words
+vim.opt.scrolloff = 8 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+-- vim.opt.guifont = 'monospace:h17' -- the font used in graphical neovim applications
+-- vim.opt.whichwrap = 'bs<>[]hl' -- which "horizontal" keys are allowed to travel to prev/next line
 
 -----------------------------------------------------------------------
-vim.opt.helplang = { 'ja', 'en' }
-
-vim.opt.showmode = false
 
 -- float-transparent
 -- vim.opt.winblend = 5
 
--- backgroundcolor
-vim.o.background = 'dark'
+-- vim.o.background = 'dark' -- Set backgroundcolor
 
 -- ファイルを開いた時に、カーソルの場所を復元する
 -- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
