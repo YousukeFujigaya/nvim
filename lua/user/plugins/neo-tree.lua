99c97a19b2ab4b4351155ba573719a0133a5210a
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2023-06-06 12:55:54 +0900

    modify: update unknown charactors for nerd fonts

--- lua/user/plugins/neo-tree.lua
+++ lua/user/plugins/neo-tree.lua
@@ -161,7 +161,7 @@ return {
             -- Change type
             added = ' ', -- or "✚", but this is redundant info if you use git_status_colors on the name
             -- modified = ' ', -- or "", but this is redundant info if you use git_status_colors on the name
-            modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+            modified = ' ', -- or "", but this is redundant info if you use git_status_colors on the name
             deleted = ' ', -- this can only be used in the git_status source
             renamed = '➜', -- this can only be used in the git_status source
             -- renamed = '', -- this can only be used in the git_status source
